5e291b8020f69f52ed8754eb7a50e3def6b2d5dc
Gaidukevich-Ivan-37 <91161027+Gaidukevich-Ivan-37@users.noreply.github.com>
2023-04-12 17:28:59 +0300

    ClassMethods

---
+++ ClassMethods/Part1/Zadanie№1/Zadanie№1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Задание__1
+{
+    class MinimumNumber
+    {   
+        static double min (double a, double b)
+        { 
+            return (a < b) ? a : b;
+        }
+        static void Main()
+        {
+            Console.WriteLine("Задание №1\n Разработать метод min (a,b) для " +
+                "\nнахождения минимального из двух чисел. Вычислить с помощью него " +
+                "\nзначение минимальное значение из чеырёх чисел x, y, z, v.\n\nРешение:");
+
+            Console.Write(" Введите радиус x: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write(" Введите высоту y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.Write(" Введите высоту z: ");
+            double z = double.Parse(Console.ReadLine());
+
+            Console.Write(" Введите высоту v: ");
+            double v = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n Минимальное значение: {0}",min(min(x, y), min(z, v)));
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClassMethods/Part1/Zadanie№2/Zadanie№2/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Задание__2
+{
+    class Program
+    {
+        static public double F(double x)
+        {
+            double y = 0;
+
+            if (Math.Abs(x) < 3)
+            {
+                y = Math.Sin(x);
+
+                Console.WriteLine("\n y = Math.Sin(x)", y);
+            }
+            else
+            {
+                if ((Math.Abs(x) >= 3) || (Math.Abs(x) < 9))
+                {
+                    y = (Math.Sqrt(Math.Pow(x, 2) + 1)) /
+                        (Math.Sqrt(Math.Pow(x, 2) + 5));
+
+                    Console.WriteLine("\n y = (Math.Sqrt(Math.Pow(x, 2) + 1)) / " +
+                        "(Math.Sqrt(Math.Pow(x, 2) + 5))", y);
+                }
+                else
+                {
+                    if (Math.Abs(x) >= 9)
+                    {
+                        y = (Math.Sqrt(Math.Pow(x, 2) + 1)) -
+                            (Math.Sqrt(Math.Pow(x, 2) + 5));
+
+                        Console.WriteLine("\n y = (Math.Sqrt(Math.Pow(x, 2) + 1)) - " +
+                        "(Math.Sqrt(Math.Pow(x, 2) + 5))", y);
+                    }
+                }
+            }
+            return y;
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Задание №2\n Построить таблицу значений функции " +
+                "\ny=f(x) для x[a, b] с шагом h.\n\nРешение:");
+
+            double a, b, h, y;
+
+            Console.Write(" Введите значение а: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write(" Введите значение b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            if (a > b)
+                Console.WriteLine("\n Значение a больше b");
+            else
+            {
+                Console.Write(" Введите значение h: ");
+                h = Convert.ToDouble(Console.ReadLine());
+
+                double s = a;
+                while (s <= (b + h))
+                {
+                    y = F(s);
+                    Console.WriteLine(" x=  " + s + "    " + "y= " + y);
+                    s += h;
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}

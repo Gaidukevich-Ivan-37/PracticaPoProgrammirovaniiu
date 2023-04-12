6c87a354d9e58931be1049b6ecffc03f3c1a51d5
Gaidukevich-Ivan-37 <91161027+Gaidukevich-Ivan-37@users.noreply.github.com>
2023-04-12 21:27:41 +0300

    HandlingExceptionalSituations

---
+++ HandlingExceptionalSituations/Part1/Zadanie№1/Zadanie№1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Задание__1
+{
+    class DivideByZeroExceptionAndFormatException
+    {
+        static void Main()
+        {
+            Console.WriteLine("Задание №1\n Вычислить значение выражений. " +
+                "\nВ каждой разработанной программе должна быть осуществлена обработка исключительных ситуаций." +
+                "\nОтдельно обработаны исключения DivideByZeroException, FormatException.\n\nРешение:");
+            
+            Console.Write(" Введите значение x: ");
+            double x = Convert.ToInt16(Console.ReadLine());
+            try
+            {
+                double y1;
+
+                if (x == -12) throw new DivideByZeroException();
+                y1 = (Math.Tan(x - 2)) / (x + 12) + (Math.Pow(Math.Cos(x / 2), 2));
+                Console.WriteLine("\n 1) y1 = (tg(x - 2)) / (x + 12) + cos^2(x / 2)= {0}", y1);
+            }
+
+            catch
+            {
+                Console.WriteLine("\n 1) Деление на ноль!");
+            }
+
+            try
+            {
+                double y2;
+
+                if (x < 0) throw new FormatException();
+                y2 = Math.Log10(x) - (Math.Atan(Math.Pow(x, 3))) / ((2 * x + 6));
+                Console.WriteLine(" 2) y2 = (1 - tg(a)) / (1 + tg(a)) = {0}\n", y2);
+            }
+
+            catch 
+            {
+                Console.WriteLine(" 2) Аргумент ln не может быть меньше или равен нулю!");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HandlingExceptionalSituations/Part1/Zadanie№3/Zadanie№3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Задание__3
+{
+    class A234
+    {
+        static void PowerA234(double A, out double B, out double C, out double D)
+        {
+            B = Math.Pow(A,2);
+            C = Math.Pow(A, 3);
+            D = Math.Pow(A, 4);
+        }
+        static void Main()
+        {
+            Console.WriteLine("Задание №3\n писать метод PowerA234(A, B, C, D), вычисляющий вторую," +
+                    "\nтретью и четвертую степень числа A и возвращающую эти степени соответственно в переменных B," +
+                    "\nC и D(A — входной, B, C, D — выходные параметры; все параметры являются вещественными)." +
+                    "\nС помощью этого метода найти вторую, третью и четвертую степень пяти данных чисел.\n\nРешение:");
+
+            for (int i = 2; i < 7; i++)
+            {
+                PowerA234(i, out double b, out double c, out double d);
+                Console.WriteLine($" {b}");
+            }
+        }
+    }
+}

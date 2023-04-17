3ed309e8110cd499ef145ff33dc7537e5694d7d6
Gaidukevich-Ivan-37 <91161027+Gaidukevich-Ivan-37@users.noreply.github.com>
2023-04-17 12:03:10 +0300

    Enumerations

---
+++ Enumerations/Part1/Zadanie№1/Zadanie№1/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace Задание__1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Задание №1\n  Описать структуру с именем STUDENT, содержащую следующие поля:" +
+                "\n - фамилия и инициалы;\n - номер группы;\n - успеваемость (массив из пяти элементов)." +
+                "\n  Написать программу, выполняющую следующие действия:" +
+                "\n - ввод с клавиатуры данных в массив, состоящий из десяти структур типа STUDENT \n" +
+                "(записи должны быть упорядочены по возрастанию номера группы);" +
+                "\n - вывод на экран фамилий и номеров групп для всех студентов, включенных в массив, если" +
+                "\n средний балл студента больше 4,0 (если таких студентов нет, вывести соответствующее сообщение)." +
+                "\n\nРешение:");
+
+            Student[] students = new Student[2];
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.Write(" Введите фамилия и инициалы {0}-го студента: ", i + 1);
+                string name = Console.ReadLine();
+
+                Console.Write(" Введите номер группы {0}-го студента: ", i + 1);
+                int number = int.Parse(Console.ReadLine());
+
+                Console.Write(" Введите, через запятую, 5 оценок {0}-го студента: ", i + 1);
+                string[] marks = Console.ReadLine().Split(',');
+
+                Console.WriteLine("-----------------------------------------------------------");
+
+                int[] progress = new int[5];
+                for (int a = 0; a < 5; a++)
+                    progress[a] = int.Parse(marks[a].ToString());
+
+
+                students[i] = new Student(name, number, progress);
+            }
+
+            var stud = from i in students orderby i.GroupNumber select i; //Сортировка
+
+            Console.WriteLine("\n Упорядоченный по номеру группы массив студентов: ");
+
+            foreach (Student student in stud)
+                Console.WriteLine(" " + student.ToString());
+
+            int count = 0;
+            foreach (Student student in students)
+                if (student.MediumBall() > 4)
+                    count++;
+
+            if (count == 0)
+                Console.WriteLine("\n Нет студентов со средним баллом больше 4!");
+            else
+                Console.WriteLine("\n Список студентов со средним баллом больше 4: ");
+
+            foreach (Student student in students)
+                if (student.MediumBall() > 4)
+                    Console.WriteLine(" " + student.ToString());
+
+            Console.ReadKey();
+        }
+    }
+
+    struct Student
+    {
+        public string Name;
+        public int GroupNumber;
+        readonly int[] progress;
+
+        public Student(string Name, int GroupNumber, int[] marks)
+        {
+            this.Name = Name;
+            this.GroupNumber = GroupNumber;
+            progress = marks;
+        }
+
+        public double MediumBall()
+        {
+            double MedBall = 0;
+
+            foreach (int i in progress)
+                MedBall += i;
+            MedBall /= progress.Length;
+
+            return MedBall;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(" ФИО: {0} \n  Номер группы: {1}", Name, GroupNumber);
+        }
+    }
+}
---
+++ Enumerations/Part1/Zadanie№2/Zadanie№2/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Задание__2
+{
+    public enum Post
+    {
+        bookkeeper = 193,
+        programmer = 124,
+        electrician = 223,
+        mechanic = 100,
+        agronomist = 99,
+        engineer = 123,
+        builder = 101
+    }
+    public class Accountant
+    {
+        private int hours;
+        public Accountant()
+        {
+            hours = 0;
+        }
+        public void Date()
+        {
+            Console.WriteLine(" Минимальное время для каждого рабочего для получения премии: \n  bookkeeper = 193; " +
+                              "\n  programmer = 124; \n  electrician = 223; \n  mechanic = 100; \n  agronomist = 99; " +
+                              "\n  engineer = 123; \n  builder = 101.\n");
+
+            foreach (string post in Enum.GetNames(typeof(Post)))
+            {
+                Console.Write($" Введите, сколько времени проработал за этот месяц {post}: ");
+                hours = Convert.ToInt32(Console.ReadLine());
+                Enum.TryParse<Post>(post, true, out Post postEnum);
+                Console.WriteLine($"  Полагается ли {post} премия: {AskForBonus(postEnum, hours)}");
+                Console.WriteLine();
+            }
+        }
+        public static bool AskForBonus(Post worker, int hours)
+        {
+            if ((int)worker <= hours)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    public class Legacy : Accountant
+    {
+        private int d;
+        public Legacy()
+        {
+            d = 0;
+        }
+        public int Legacy_Result()
+        {
+            int a = 12;
+            int b = 0;
+            int oneYears = 0;
+            int[] enums = (int[])Enum.GetValues(typeof(Post));
+            int sizeEnum = Enum.GetNames(typeof(Post)).Length;
+            for (int i = 0; i < sizeEnum; i++)
+            {
+                d = enums[i];
+                oneYears += a * (b + d);
+            }
+            return oneYears;
+        }
+    }
+    public class ProgramMain
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Задание №1\n Создайте перечисление, в котором будут содержаться должности" +
+                "\n сотрудников как имена констант. Присвойте каждой константе значение," +
+                "\n задающее количество часов, которые должен отработать сотрудник за месяц.\n\nРешение:");
+
+            Accountant acc = new();
+            acc.Date();
+
+            Legacy l = new();
+            Console.WriteLine($"{l.Legacy_Result()} Часов в год на работников.");
+
+            Console.ReadKey();
+        }
+    }
+}

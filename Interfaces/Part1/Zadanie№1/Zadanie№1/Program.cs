4bfc337351b9798d400e0e8a64082b2e236d6bea
Gaidukevich-Ivan-37 <91161027+Gaidukevich-Ivan-37@users.noreply.github.com>
2023-04-18 20:35:16 +0300

    Interfaces

---
+++ Interfaces/Part1/Zadanie№1/Zadanie№1/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace Задание__1
+{
+    interface Ix
+    {
+        void IxF0(double xKey);
+        void IxF1();
+    }
+    interface Iy
+    {
+        void F0(double xKey);
+        void F1();
+    }
+    interface Iz
+    {
+        void F0(double xKey);
+        void F1();
+    }
+    class TestClass : Ix, Iy, Iz
+    {
+        public double xVal;
+        public TestClass()
+        {
+            xVal = 125;
+        }
+        public TestClass(double key)
+        {
+            xVal = key;
+        }
+        public void IxF0(double key)
+        {
+            xVal = Math.Cos(key); Console.WriteLine(" IxF0({0})...", xVal);
+        }
+        public void IxF1()
+        {
+            xVal = Math.Cos(xVal);
+            Console.WriteLine(" IxF1({0})...", xVal);
+        }
+        public void F0(double xKey)
+        {
+            xVal = Math.Exp(xVal);
+            Console.WriteLine(" (Exp(Iy))F0({0})...", xVal);
+        }
+        public void F1()
+        {
+            xVal = Math.Exp(xVal);
+            Console.WriteLine(" (Exp(Iy))F1({0})...", xVal);
+        }
+        void Iz.F0(double xKey)
+        {
+            xVal = 1/Math.Exp(xKey);
+            Console.WriteLine(" Iz.F0({0})...", xVal);
+        }
+        void Iz.F1()
+        {
+            xVal = 1 / Math.Exp(xVal);
+            Console.WriteLine(" Iz.F1({0})...", xVal);
+        }
+        public void bF0()
+
+        {
+            Console.WriteLine(" bF0()...");
+        }
+        class Interfaces
+        {
+            static void Main()
+            {
+                Console.WriteLine("Задание №1\n Параметр (double).\n IxF0, IxF1 возвращают (Cos(w))." +
+                    "\n F0 F1 возвращают:\n  -неявная реализация (Exp(w)),\n  -явная реализация Iz (1/Exp(w))." +
+                    "\n\nРешение:");
+
+                TestClass x0 = new TestClass();
+                TestClass x1 = new TestClass(5);
+                x0.bF0();
+
+                x0.IxF0(10);
+                x1.IxF1();
+
+                x0.F0(5);
+                x1.F1();
+
+                (x0 as Iy).F0(7);
+                (x1 as Iz).F1();
+
+                Console.WriteLine(" ==========Prism test==========");
+                Console.WriteLine(" ==========Ix==========");
+                Ix ix = x1;
+                ix.IxF0(5);
+                ix.IxF1();
+
+                Console.WriteLine(" ==========Iy==========");
+                Iy iy = x1;
+                iy.F0(5);
+                iy.F1();
+
+                Console.WriteLine(" ==========Iz==========");
+                Iz iz = x1;
+                iz.F0(5);
+                iz.F1();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Interfaces/Part1/Zadanie№2/Zadanie№2/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Задание__2
+{
+    public class Interfaces
+    {
+        static void Main()
+        {
+            Console.WriteLine("Задание №2\n Описать класс «автостоянка» для хранения сведений об автомобилях. " +
+                "\n Для каждого автомобиля записываются госномер, цвет, " +
+                "\n фамилия владельца и признак присутствия на стоянке.\n\nРешение:");
+
+            Auto audi = new Auto();
+            audi.Model = " Audi:";
+            audi.Color = "red";
+            audi.Number = "AS4566N";
+            audi.Time = DateTime.Now.AddDays(-1);
+
+            Auto mers = new Auto();
+            mers.Model = " Mersedes:";
+            mers.Color = "black";
+            mers.Number = "NS6456A";
+            mers.Time = DateTime.Now;
+
+            Auto bmw = new Auto();
+            bmw.Model = " Bmw:";
+            bmw.Color = "cyan";
+            bmw.Number = "DF3333H";
+            bmw.Time = DateTime.Now.AddYears(-1);
+            audi.toString();
+            Console.WriteLine(audi.getNumber());
+            mers.toString();
+            Console.WriteLine(mers.getNumber());
+            bmw.toString();
+            Console.WriteLine(bmw.getNumber());
+        }
+    }
+    class Auto : Stend
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string Number { get; set; }
+        public DateTime Time { get; set; }
+        private string AutoName;
+        public void toString()
+        {
+            AutoName = Model + "\n  -цвет: " + Color + "\n  -номера: " + Number + "\n  -время прибытия: " + Time;
+            printToDisplay();
+        }
+        public void printToDisplay()
+        {
+            Console.WriteLine(AutoName);
+        }
+        public string getNumber()
+        {
+            return (DateTime.Now - Time).TotalDays > 100 ? " Должник\n" : " Не должен\n";
+        }
+    }
+    interface Stend
+    {
+        void printToDisplay();
+        string getNumber();
+    }
+}

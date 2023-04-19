5c2bfb3a29cd2b11502e766ab330f7eb3d6e4882
Gaidukevich-Ivan-37 <91161027+Gaidukevich-Ivan-37@users.noreply.github.com>
2023-04-19 21:21:22 +0300

    InheritanceMechanism

---
+++ InheritanceMechanism/Part1/Zadanie№1/Zadanie№1/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Задание__1
+{
+    class Notebook
+    {
+        private string? name;
+        private double k;
+        private double income;
+        public Notebook()
+        {
+            name = null;
+            k = 0;
+            income = 0;
+            Input();
+        }
+        public virtual void IncomePay()
+        {
+            income = 15 * k;
+        }
+        public string Name { get { return name; } set { name = value; } }
+        public double K { get { return k; } set { k = value; } }
+        public double Income { get { return income; } set { income = value; } }
+        public void Input()
+        {
+            Console.Write(" Введите название тетради: ", name);
+            name = Console.ReadLine();
+
+            Console.Write(" Введите количество страниц: ", k);
+            k = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+    class General : Notebook
+    {
+        int n;
+        public General() : base()
+        {
+            n = 0;
+            Input();
+        }
+        public new void Input()
+        {
+            Console.Write(" Введите количество материала обложки: ");
+            n = Convert.ToInt32(Console.ReadLine());
+        }
+        public override void IncomePay() 
+        {
+            Income = 15 * K * n + 50;
+        }
+        public void GetInfo() 
+        {
+            Console.WriteLine("\n Название: {0} \n Количество страниц: {1} \n Материала обложки: {2} " +
+                "\n\n Доход: {3} ", Name, K, n, Income);
+        }
+    }
+    class Program 
+    { 
+        static void Main()
+        {
+            Console.WriteLine("Задание №1\n Базовый класс: Тетрадь (поле: название, количество листов –k). " +
+                "Метод: Стоимость: 15 * k. Потомок: Общая (поле – материал обложки). Изменения в потомках: " +
+                "Изменить стоимость с учетом надбавки за обложку на 50р.\n\nРешение:");
+
+            General general = new();
+            general.IncomePay();
+            general.GetInfo();
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ InheritanceMechanism/Part1/Zadanie№2/Zadanie№2/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Задание__2
+{
+    class A
+    {
+        private double a;
+        private double b;
+        private double c;
+        private double c1;
+        public A()
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+            c1 = 0;
+            Input();
+        }
+        public virtual void IncomePay()
+        {
+            c = a + b;
+        }
+        public double A1 { get { return a; } set { a = value; } }
+        public double B1 { get { return b; } set { b = value; } }
+        public double C1 { get { return c; } set { c = value; } }
+        public double C2 { get { return c1; } set { c1 = value; } }
+        public void Input()
+        {
+            Console.Write(" Введите число a: ", a);
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write(" Введите число b: ", b);
+            b = Convert.ToDouble(Console.ReadLine());
+        }
+        class B : A
+        {
+            double d;
+            public B() : base()
+            {
+                d = 0;
+                Input();
+            }
+            public new void Input()
+            {
+                Console.Write(" Введите число d: ");
+                d = Convert.ToInt32(Console.ReadLine());
+            }
+            public override void IncomePay()
+            {
+                C1 = A1 + B1;
+                C2 = A1 + B1 + d;
+            }
+
+            public void GetInfo()
+            {
+                if (C1 == C2)
+                    Console.WriteLine("\n С1 = С2");
+                else
+                {
+                    Console.WriteLine("\n a + b = {0}", C1);
+                    Console.WriteLine(" a + b + d = {0}", C2);
+                }
+            }
+        }
+        class Program
+        {
+            static void Main()
+            {
+                Console.WriteLine("Задание №2\n В рамках консольного приложения разработать класс " +
+                    "\n В-наследник класса А(класс А с полями а и b и свойством с. " +
+                    "\n Свойство – значение выражения над полями а и b(выражение и типы полей – см. вариант в таблице 1). " +
+                    "\n Поля инициализировать при объявлении класса. Конструктор оставить по умолчанию. " +
+                    "\n Проследить, чтобы поля а и b напрямую в других классах были недоступны.) с полем d и свойством с2." +
+                    "\n\nРешение:");
+
+                B b1 = new();
+                b1.IncomePay();
+                b1.GetInfo();
+
+                Console.ReadKey();
+            }
+        }
+    } 
+}

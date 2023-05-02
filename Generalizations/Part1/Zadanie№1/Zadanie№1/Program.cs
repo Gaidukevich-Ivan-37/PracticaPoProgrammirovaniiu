c1e77641096f25adb5dab2dddd9bfb1de3f55cbf
Gaidukevich-Ivan-37 <91161027+Gaidukevich-Ivan-37@users.noreply.github.com>
2023-05-02 23:02:49 +0300

    Generalizations

---
+++ Generalizations/Part1/Zadanie№1/Zadanie№1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Задание__1
+{
+    class MyList<T>
+    {
+        private List<T> items;
+        public MyList()
+        {
+            items = new List<T>();
+        }
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+        public T this[int index]
+        {
+            get {return items[index];}
+            set {items[index] = value;}
+        }
+        public int Count
+        {
+            get {return items.Count;}
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Задание №1\n Создайте класс MyList<T>. Реализуйте возможность использования его экземпляра " +
+                "\n аналогично экземпляру класса List<T>. \n\nРешение:");
+
+            MyList<int> myList = new MyList<int>();
+
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            
+            int i = 2;
+            int item = myList[i];
+            int count = myList.Count;
+
+            Console.WriteLine($" Пункт с индексом {i}: {item}");
+            Console.WriteLine($" Всего элементов в списке: {count}");
+        }
+    }
+}
---
+++ Generalizations/Part1/Zadanie№2/Zadanie№2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Задание__2
+{
+    class MyDictionary<TKey, TValue>
+    {
+        private List<KeyValuePair<TKey, TValue>> _items = new List<KeyValuePair<TKey, TValue>>();
+
+        public void Add(TKey key, TValue value)
+        {
+            _items.Add(new KeyValuePair<TKey, TValue>(key, value));
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                foreach (var item in _items)
+                {
+                    if (item.Key.Equals(key))
+                    {
+                        return item.Value;
+                    }
+                }
+                throw new KeyNotFoundException();
+            }
+        }
+
+        public int Count
+        {
+            get { return _items.Count; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание №2\n Создайте класс MyDictionary <TKey, TValue>. Реализуйте возможность использования его экземпляра " +
+                "\n аналогично экземпляру класса Dictionary. \n\nРешение:");
+
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("key1", "value1");
+            myDictionary.Add("key2", "value2");
+            Console.WriteLine(myDictionary["key1"]);
+            Console.WriteLine(myDictionary.Count); 
+        }
+    }
+}

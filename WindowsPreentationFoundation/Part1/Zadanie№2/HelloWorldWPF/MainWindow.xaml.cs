162d2debb582371031cf70442aa975f2f05ea24e
Gaidukevich-Ivan-37 <91161027+Gaidukevich-Ivan-37@users.noreply.github.com>
2023-05-18 15:45:29 +0300

    WindowsPreentationFoundation

---
+++ WindowsPreentationFoundation/Part1/Zadanie№2/HelloWorldWPF/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorldWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            buttonRun.Click += buttonRun_Click;
+            buttonAbout.Click += buttonAbout_Click;
+        }
+        private void buttonAbout_Click(object sender, RoutedEventArgs e)
+        {
+            string info = $"О программе {this.Title}";
+            AboutApp aboutApp = new AboutApp(info);
+            aboutApp.ShowDialog();
+        }
+        private void buttonRun_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxEnterName.Text = textBoxEnter.Text;
+        }
+        private void buttonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void textBoxEnter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string input = string.Empty;
+            input = textBoxEnter.Text;
+        }
+    }
+}

e906ffa637826f2cc7774732e6d089c05a640f80
Gaidukevich-Ivan-37 <91161027+Gaidukevich-Ivan-37@users.noreply.github.com>
2023-05-19 01:04:48 +0300

    WorkingWithSqlDataAdapterAndDataSet

---
+++ WorkingWithSqlDataAdapterAndDataSet/Part1/Part1/WindowsFormsApplication1/Models/Turist.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq.Mapping;
+using System.Data.OleDb;
+
+namespace WindowsFormsApplication1.Models
+{
+    [Table(Name="Туристы")]
+    class Turist
+    {
+        [Column(Name = "Код", IsPrimaryKey = true, IsDbGenerated = true)]
+
+        public int Id { get; set; }
+
+        [Column(Name = "Имя")]
+        public string FirstName { get; set; }
+
+        [Column(Name = "Отчество")]
+        public string MidleName { get; set; }
+
+        [Column(Name = "Фамилия")]
+        public string LastName { get; set; }
+
+    }
+}
---
+++ WorkingWithSqlDataAdapterAndDataSet/Part1/Part1/WindowsFormsApplication1/Models/TuristyDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+
+namespace WindowsFormsApplication1.Models
+{
+    class TuristyDBContext: DataContext
+    {
+        public TuristyDBContext(string fileOrServerConnection):
+            base(fileOrServerConnection)
+        { 
+            
+        }
+
+        public Table<Turist> Turists { get; set; }
+
+
+    }
+}

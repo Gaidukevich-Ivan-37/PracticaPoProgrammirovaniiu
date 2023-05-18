9b43d3cd0b3c35a728c5d31eaf837a4f33420da4
Gaidukevich-Ivan-37 <91161027+Gaidukevich-Ivan-37@users.noreply.github.com>
2023-05-18 16:29:41 +0300

    WorkingWithGraphics

---
+++ WorkingWithGraphics/Part1/Zadanie№1-2/Zadanie№1/TestDrawing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace DrawingContext
+{
+    class TestDrawing:Control
+    {
+        protected override void OnRender(System.Windows.Media.DrawingContext drawingContext)
+        {
+            drawingContext.DrawEllipse(Brushes.Transparent, new Pen(Brushes.Black, 1), new Point(50, 50), 40, 40);
+            drawingContext.DrawEllipse(Brushes.Transparent, new Pen(Brushes.Black, 1), new Point(50, 50), 30, 30);
+            drawingContext.DrawEllipse(Brushes.Transparent, new Pen(Brushes.Black, 1), new Point(50, 50), 15, 15);
+
+            drawingContext.DrawRectangle(Brushes.Transparent, new Pen(Brushes.Black, 1), new Rect(100,10,40,60));
+            drawingContext.DrawRectangle(Brushes.Transparent, new Pen(Brushes.Black, 1), new Rect(120, 50, 40, 60));
+            drawingContext.DrawRectangle(Brushes.Transparent, new Pen(Brushes.Black, 1), new Rect(140, 90, 40, 60));
+            drawingContext.DrawRectangle(Brushes.Transparent, new Pen(Brushes.Black, 1), new Rect(160, 130, 40, 60));
+
+            base.OnRender(drawingContext);
+        }
+    }
+}

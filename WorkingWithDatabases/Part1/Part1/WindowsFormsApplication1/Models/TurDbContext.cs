7be56fcafd7a21ad3edd82e013961eb931446299
Gaidukevich-Ivan-37 <91161027+Gaidukevich-Ivan-37@users.noreply.github.com>
2023-05-19 00:47:44 +0300

    WorkingWithDatabases

---
+++ WorkingWithDatabases/Part1/Part1/WindowsFormsApplication1/Models/TurDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+
+namespace WindowsFormsApplication1.Models
+{
+    class TurDbContext : DataContext
+    {
+        public TurDbContext(string fileOrServerConnection) :
+            base(fileOrServerConnection)
+        { 
+            
+        }
+
+        public Table<Tur> Turists { get; set; }
+
+    }
+}

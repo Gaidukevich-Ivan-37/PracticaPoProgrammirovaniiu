74fb7f7eece29362ad1495b6c12aced7655c44e5
Gaidukevich-Ivan-37 <91161027+Gaidukevich-Ivan-37@users.noreply.github.com>
2023-05-19 00:52:45 +0300

    C#AndMSSQLServer

---
+++ C#AndMSSQLServer/Part1/Part1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        public static string connectString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=turfirm.mdb;";
+
+        private OleDbConnection myConnection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            myConnection = new OleDbConnection(connectString);
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string query = "Select * from Туры";
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(query, myConnection);
+            DataSet ds = new DataSet();
+            dataAdapter.Fill(ds, "Туры");
+            dataGridView1.DataSource = ds.Tables[0].DefaultView;
+
+            string query2 = "Select * from Туристы";
+            OleDbDataAdapter dataAdapter2 = new OleDbDataAdapter(query2, myConnection);
+            DataSet ds2 = new DataSet();
+            dataAdapter2.Fill(ds2, "Туристы");
+            dataGridView2.DataSource = ds2.Tables[0].DefaultView;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myConnection.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count != 0)
+            {
+                var id = dataGridView1[0, dataGridView1.CurrentRow.Index].Value.ToString();
+
+                myConnection.Open();
+                string query = "DELETE FROM Туры where Код=" + id;
+                OleDbCommand command = new OleDbCommand(query, myConnection);
+                command.ExecuteNonQuery();
+                myConnection.Close();
+                dataGridView1.DataSource = null;
+                string q = "Select * from Туры";
+                OleDbDataAdapter dataAdapter = new OleDbDataAdapter(q, myConnection);
+                DataSet ds = new DataSet();
+                dataAdapter.Fill(ds, "Туры");
+                dataGridView1.DataSource = ds.Tables[0].DefaultView;
+
+            }
+            else
+                MessageBox.Show("Выберите строку для удаления!", "Ошибка");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string surName = textBox2.Text;
+            string otche = textBox3.Text; 
+
+
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "")
+            {
+                MessageBox.Show("Поля ввода пусты!", "Ошибка");
+            }
+            else
+            {
+                myConnection.Open();
+                string query = "INSERT INTO Туристы (Имя, Фамилия, Отчество) VALUES ('" + name + "', '" + surName + "', '" + otche + "')";
+                OleDbCommand command = new OleDbCommand(query, myConnection);
+                command.ExecuteNonQuery();
+                myConnection.Close();
+                dataGridView2.DataSource = null;
+                string q = "Select * from Туристы";
+                OleDbDataAdapter dataAdapter = new OleDbDataAdapter(q, myConnection);
+                DataSet ds = new DataSet();
+                dataAdapter.Fill(ds, "Туристы");
+                dataGridView2.DataSource = ds.Tables[0].DefaultView;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string surName = textBox2.Text;
+            string otche = textBox3.Text; 
+
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "")
+            {
+                MessageBox.Show("Поля ввода пусты!", "Ошибка");
+            }
+            else
+            {
+                if (dataGridView2.SelectedRows.Count != 0)
+                {
+                    myConnection.Open();
+                    var id = dataGridView2[0, dataGridView2.CurrentRow.Index].Value.ToString();
+                    string query = "UPDATE Туристы SET Имя='" + name + "', Фамилия='" + surName + "' , Отчество='" + otche + "' where Код=" + id;
+                    OleDbCommand command = new OleDbCommand(query, myConnection);
+                    command.ExecuteNonQuery();
+                    myConnection.Close();
+                    dataGridView2.DataSource = null;
+                    string q = "Select * from Туристы";
+                    OleDbDataAdapter dataAdapter = new OleDbDataAdapter(q, myConnection);
+                    DataSet ds = new DataSet();
+                    dataAdapter.Fill(ds, "Туристы");
+                    dataGridView2.DataSource = ds.Tables[0].DefaultView;
+                }
+                
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (dataGridView2.SelectedRows.Count != 0)
+            {
+                var id = dataGridView2[0, dataGridView2.CurrentRow.Index].Value.ToString();
+
+                myConnection.Open();
+                string query = "DELETE FROM Туристы where Код=" + id;
+                OleDbCommand command = new OleDbCommand(query, myConnection);
+                command.ExecuteNonQuery();
+                myConnection.Close();
+                dataGridView2.DataSource = null;
+                string q = "Select * from Туристы";
+                OleDbDataAdapter dataAdapter = new OleDbDataAdapter(q, myConnection);
+                DataSet ds = new DataSet();
+                dataAdapter.Fill(ds, "Туристы");
+                dataGridView2.DataSource = ds.Tables[0].DefaultView;
+
+            }
+            else
+                MessageBox.Show("Выберите строку для удаления!", "Ошибка");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string name = textBox4.Text;
+            int zena = int.Parse(textBox5.Text);
+            string info = textBox6.Text;
+
+
+            if (textBox4.Text == "" || textBox5.Text == "" || textBox6.Text == "")
+            {
+                MessageBox.Show("Поля ввода пусты!", "Ошибка");
+            }
+            else
+            {
+                myConnection.Open();
+                string query = "INSERT INTO Туры (Название, Цена, Информация) VALUES ('" + name + "', '" + zena + "', '" + info + "')";
+                OleDbCommand command = new OleDbCommand(query, myConnection);
+                command.ExecuteNonQuery();
+                myConnection.Close();
+                dataGridView1.DataSource = null;
+                string q = "Select * from Туры";
+                OleDbDataAdapter dataAdapter = new OleDbDataAdapter(q, myConnection);
+                DataSet ds = new DataSet();
+                dataAdapter.Fill(ds, "Туры");
+                dataGridView1.DataSource = ds.Tables[0].DefaultView;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = textBox4.Text;
+                int zena = int.Parse(textBox5.Text);
+                string info = textBox6.Text;
+
+                if (textBox4.Text == "" || textBox5.Text == "" || textBox6.Text == "")
+                {
+                    MessageBox.Show("Поля ввода пусты!", "Ошибка");
+                }
+                else
+                {
+                    if (dataGridView1.SelectedRows.Count != 0)
+                    {
+                        myConnection.Open();
+                        var id = dataGridView1[0, dataGridView1.CurrentRow.Index].Value.ToString();
+                        string query = "UPDATE Туры SET Название='" + name + "', Цена='" + zena + "' , Информация='" + info + "' where Код=" + id;
+                        OleDbCommand command = new OleDbCommand(query, myConnection);
+                        command.ExecuteNonQuery();
+                        myConnection.Close();
+                        dataGridView1.DataSource = null;
+                        string q = "Select * from Туры";
+                        OleDbDataAdapter dataAdapter = new OleDbDataAdapter(q, myConnection);
+                        DataSet ds = new DataSet();
+                        dataAdapter.Fill(ds, "Туры");
+                        dataGridView1.DataSource = ds.Tables[0].DefaultView;
+                    }
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка, проверьте введенные данные!", "Ошибка");
+            }
+          
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            f.Show();
+        }
+    }
+}
---
+++ C#AndMSSQLServer/Part1/Part1/WindowsFormsApplication1/Form2.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.турыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.туристыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сезоныToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.путевкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оплатаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.информацияОТуристахToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.турыToolStripMenuItem,
+            this.туристыToolStripMenuItem,
+            this.сезоныToolStripMenuItem,
+            this.путевкиToolStripMenuItem,
+            this.оплатаToolStripMenuItem,
+            this.информацияОТуристахToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(736, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // турыToolStripMenuItem
+            // 
+            this.турыToolStripMenuItem.Name = "турыToolStripMenuItem";
+            this.турыToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
+            this.турыToolStripMenuItem.Text = "Туры";
+            this.турыToolStripMenuItem.Click += new System.EventHandler(this.турыToolStripMenuItem_Click);
+            // 
+            // туристыToolStripMenuItem
+            // 
+            this.туристыToolStripMenuItem.Name = "туристыToolStripMenuItem";
+            this.туристыToolStripMenuItem.Size = new System.Drawing.Size(78, 24);
+            this.туристыToolStripMenuItem.Text = "Туристы";
+            this.туристыToolStripMenuItem.Click += new System.EventHandler(this.туристыToolStripMenuItem_Click);
+            // 
+            // сезоныToolStripMenuItem
+            // 
+            this.сезоныToolStripMenuItem.Name = "сезоныToolStripMenuItem";
+            this.сезоныToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.сезоныToolStripMenuItem.Text = "Сезоны";
+            this.сезоныToolStripMenuItem.Click += new System.EventHandler(this.сезоныToolStripMenuItem_Click);
+            // 
+            // путевкиToolStripMenuItem
+            // 
+            this.путевкиToolStripMenuItem.Name = "путевкиToolStripMenuItem";
+            this.путевкиToolStripMenuItem.Size = new System.Drawing.Size(77, 24);
+            this.путевкиToolStripMenuItem.Text = "Путевки";
+            this.путевкиToolStripMenuItem.Click += new System.EventHandler(this.путевкиToolStripMenuItem_Click);
+            // 
+            // оплатаToolStripMenuItem
+            // 
+            this.оплатаToolStripMenuItem.Name = "оплатаToolStripMenuItem";
+            this.оплатаToolStripMenuItem.Size = new System.Drawing.Size(71, 24);
+            this.оплатаToolStripMenuItem.Text = "Оплата";
+            this.оплатаToolStripMenuItem.Click += new System.EventHandler(this.оплатаToolStripMenuItem_Click);
+            // 
+            // информацияОТуристахToolStripMenuItem
+            // 
+            this.информацияОТуристахToolStripMenuItem.Name = "информацияОТуристахToolStripMenuItem";
+            this.информацияОТуристахToolStripMenuItem.Size = new System.Drawing.Size(190, 24);
+            this.информацияОТуристахToolStripMenuItem.Text = "Информация о туристах";
+            this.информацияОТуристахToolStripMenuItem.Click += new System.EventHandler(this.информацияОТуристахToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(700, 385);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 445);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form2";
+            this.Text = "Все таблицы";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem турыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem туристыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сезоныToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem путевкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оплатаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem информацияОТуристахToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}

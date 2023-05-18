98adbaf42575947ec775dfd15fe402faccb0c5ee
Gaidukevich-Ivan-37 <91161027+Gaidukevich-Ivan-37@users.noreply.github.com>
2023-05-19 01:29:15 +0300

    WindowsAppDeployment

---
+++ WindowsAppDeployment/Part1/Part1/WindowsFormsApplication1/Models/Tur.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq.Mapping;
+using System.Data.OleDb;
+
+namespace WindowsFormsApplication1.Models
+{
+    [Table(Name = "Туры")]
+    class Tur
+    {
+        [Column(Name = "Код", IsPrimaryKey = true, IsDbGenerated = true)]
+
+        public int Id { get; set; }
+
+        [Column(Name = "Название")]
+        public string Title { get; set; }
+
+        [Column(Name = "Цена")]
+        public string Zena { get; set; }
+
+        [Column(Name = "Информация")]
+        public string Info { get; set; }
+    }
+}
